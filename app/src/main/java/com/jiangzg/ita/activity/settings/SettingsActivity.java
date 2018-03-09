3349390fd331ccf3d2f3e83b66d032eb23e9155b
蒋治国 <18600757185@163.com>
2018-03-09 17:02:09 +0800

    Update settings

--- app/src/main/java/com/jiangzg/ita/activity/settings/SettingsActivity.java
+++ app/src/main/java/com/jiangzg/ita/activity/settings/SettingsActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.jiangzg.base.component.activity.ActivityTrans;
 import com.jiangzg.ita.R;
@@ -11,12 +15,62 @@ import com.jiangzg.ita.base.BaseActivity;
 import com.jiangzg.ita.utils.ViewUtils;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SettingsActivity extends BaseActivity<SettingsActivity> {
 
     @BindView(R.id.tb)
     Toolbar tb;
+    @BindView(R.id.tvTheme)
+    TextView tvTheme;
+    @BindView(R.id.switchWifi)
+    Switch switchWifi;
+    @BindView(R.id.rlWifi)
+    RelativeLayout rlWifi;
+    @BindView(R.id.switchDownland)
+    Switch switchDownland;
+    @BindView(R.id.rlDownload)
+    RelativeLayout rlDownload;
+    @BindView(R.id.tvCacheSummary)
+    TextView tvCacheSummary;
+    @BindView(R.id.switchCache)
+    Switch switchCache;
+    @BindView(R.id.rlCache)
+    RelativeLayout rlCache;
+    @BindView(R.id.switchSystem)
+    Switch switchSystem;
+    @BindView(R.id.rlSystem)
+    RelativeLayout rlSystem;
+    @BindView(R.id.switchTa)
+    Switch switchTa;
+    @BindView(R.id.rlTa)
+    RelativeLayout rlTa;
+    @BindView(R.id.switchOther)
+    Switch switchOther;
+    @BindView(R.id.rlOther)
+    RelativeLayout rlOther;
+    @BindView(R.id.tvPhone)
+    TextView tvPhone;
+    @BindView(R.id.tvPassword)
+    TextView tvPassword;
+    @BindView(R.id.tvUpdateSummary)
+    TextView tvUpdateSummary;
+    @BindView(R.id.rlUpdate)
+    RelativeLayout rlUpdate;
+    @BindView(R.id.tvHelp)
+    TextView tvHelp;
+    @BindView(R.id.tvSuggest)
+    TextView tvSuggest;
+    @BindView(R.id.tvRate)
+    TextView tvRate;
+    @BindView(R.id.tvOpen)
+    TextView tvOpen;
+    @BindView(R.id.tvProtocol)
+    TextView tvProtocol;
+    @BindView(R.id.tvContact)
+    TextView tvContact;
+    @BindView(R.id.tvExist)
+    TextView tvExist;
 
     public static void goActivity(Activity from) {
         Intent intent = new Intent(from, SettingsActivity.class);
@@ -39,4 +93,43 @@ public class SettingsActivity extends BaseActivity<SettingsActivity> {
 
     }
 
+    @OnClick({R.id.tvTheme, R.id.rlWifi, R.id.rlDownload, R.id.rlCache, R.id.rlSystem, R.id.rlTa, R.id.rlOther, R.id.tvPhone, R.id.tvPassword, R.id.rlUpdate, R.id.tvHelp, R.id.tvSuggest, R.id.tvRate, R.id.tvOpen, R.id.tvProtocol, R.id.tvContact, R.id.tvExist})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tvTheme:
+                break;
+            case R.id.rlWifi:
+                break;
+            case R.id.rlDownload:
+                break;
+            case R.id.rlCache:
+                break;
+            case R.id.rlSystem:
+                break;
+            case R.id.rlTa:
+                break;
+            case R.id.rlOther:
+                break;
+            case R.id.tvPhone:
+                break;
+            case R.id.tvPassword:
+                break;
+            case R.id.rlUpdate:
+                break;
+            case R.id.tvHelp:
+                break;
+            case R.id.tvSuggest:
+                break;
+            case R.id.tvRate:
+                break;
+            case R.id.tvOpen:
+                break;
+            case R.id.tvProtocol:
+                break;
+            case R.id.tvContact:
+                break;
+            case R.id.tvExist:
+                break;
+        }
+    }
 }
