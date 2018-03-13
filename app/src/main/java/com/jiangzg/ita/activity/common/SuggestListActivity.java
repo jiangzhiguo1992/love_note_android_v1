977756992beabf1b1599f1f853c65705d470051a
蒋治国 <18600757185@163.com>
2018-03-13 23:37:01 +0800

    Update suggestadapter

--- app/src/main/java/com/jiangzg/ita/activity/common/SuggestListActivity.java
+++ app/src/main/java/com/jiangzg/ita/activity/common/SuggestListActivity.java
@@ -3,17 +3,20 @@ package com.jiangzg.ita.activity.common;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemChildClickListener;
 import com.jiangzg.base.component.activity.ActivityTrans;
 import com.jiangzg.ita.R;
 import com.jiangzg.ita.adapter.SuggestAdapter;
 import com.jiangzg.ita.base.BaseActivity;
 import com.jiangzg.ita.base.MyApp;
 import com.jiangzg.ita.domain.Suggest;
+import com.jiangzg.ita.third.RecyclerManager;
 import com.jiangzg.ita.utils.ViewUtils;
 import com.jiangzg.ita.view.GSwipeRefreshLayout;
 
@@ -30,6 +33,7 @@ public class SuggestListActivity extends BaseActivity<SuggestListActivity> {
     GSwipeRefreshLayout srl;
     @BindView(R.id.rv)
     RecyclerView rv;
+    private RecyclerManager recyclerManager;
 
     public static void goActivity(Activity from) {
         Intent intent = new Intent(from, SuggestListActivity.class);
@@ -46,98 +50,133 @@ public class SuggestListActivity extends BaseActivity<SuggestListActivity> {
     protected void initView(Bundle state) {
         ViewUtils.initTopBar(mActivity, tb, getString(R.string.suggest_feedback), true);
         // recycler
-        rv.setLayoutManager(new LinearLayoutManager(mActivity));
-        SuggestAdapter adapter = new SuggestAdapter(mActivity);
-        rv.setAdapter(adapter);
+        //rv.setLayoutManager(new LinearLayoutManager(mActivity));
+        //SuggestAdapter adapter = new SuggestAdapter(mActivity);
+        //rv.setAdapter(adapter);
         // refresh
         //srl.setProgressViewEndTarget();
+        //srl.onInterceptTouchEvent();
+        //srl.offsetTopAndBottom();
+        //srl.setOnDragListener(new View.OnDragListener() {
+        //    @Override
+        //    public boolean onDrag(View v, DragEvent event) {
+        //        return false;
+        //    }
+        //});
+        //srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        //    @Override
+        //    public void onRefresh() {
+        //        getData(true);
+        //    }
+        //});
 
-        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getData(true);
-            }
-        });
-
-        // todo swipe
+        recyclerManager = new RecyclerManager(mActivity)
+                .initRecycler(rv)
+                .initLayoutManager(new LinearLayoutManager(mActivity))
+                .initRefresh(srl)
+                .setAdapter(new SuggestAdapter(mActivity))
+                .viewHeader(R.layout.list_head_suggest) // todo head
+                //.viewLoading() // todo load
+                //.viewEmpty() // todo empty
+                .listenerRefresh(new RecyclerManager.RefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        getData(false);
+                    }
+                })
+                .listenerMore(new RecyclerManager.MoreListener() {
+                    @Override
+                    public void onMore(int currentCount) {
+                        getData(true);
+                    }
+                })
+                .listenerClick(new OnItemChildClickListener() {
+                    @Override
+                    public void onSimpleItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                        SuggestAdapter suggestAdapter = (SuggestAdapter) adapter;
+                        switch (view.getId()) {
+                            case R.id.llTitle:
+                                suggestAdapter.goSuggestDetail(position);
+                                break;
+                            case R.id.llWatch:
+                                suggestAdapter.toggleWatch(position);
+                                break;
+                            case R.id.llComment:
+                                suggestAdapter.comment(position);
+                                break;
+                        }
+                    }
+                });
         // todo behavior
     }
 
     @Override
     protected void initData(Bundle state) {
-        getData(true);
+        recyclerManager.dataRefresh();
     }
 
 
-    public void getData(final boolean refresh) {
-        if (!srl.isRefreshing()) {
-            srl.setRefreshing(true);
-        }
+    public void getData(final boolean more) {
+        // todo api
         MyApp.get().getHandler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 srl.setRefreshing(false);
-                getDataByHttp(refresh);
+                if (more) {
+                    Suggest s0 = new Suggest();
+                    s0.setTitle("下拉出来的！");
+                    s0.setCreatedAt(1520866299);
+                    s0.setUpdatedAt(1520866299);
+                    //s1.setContent();
+                    s0.setWatch(false);
+                    s0.setComment(false);
+                    s0.setWatchCount(0);
+                    s0.setCommentCount(0);
+                    List<Suggest> list = new ArrayList<>();
+                    list.add(s0);
+                    recyclerManager.data(list, more);
+                } else {
+                    List<Suggest> suggestList = new ArrayList<>();
+                    Suggest s1 = new Suggest();
+                    s1.setTitle("我发现了一个bug！");
+                    s1.setCreatedAt(1520866299);
+                    s1.setUpdatedAt(1520866299);
+                    //s1.setContent();
+                    s1.setWatch(false);
+                    s1.setComment(false);
+                    s1.setWatchCount(0);
+                    s1.setCommentCount(0);
+                    Suggest s2 = new Suggest();
+                    s2.setTitle("我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！");
+                    s2.setCreatedAt(1520010299);
+                    s2.setUpdatedAt(1520866299);
+                    //s2.setContent();
+                    s2.setWatch(true);
+                    s2.setComment(false);
+                    s2.setWatchCount(111111111);
+                    s2.setCommentCount(0);
+                    Suggest s3 = new Suggest();
+                    s3.setTitle("我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！");
+                    s3.setCreatedAt(1520010299);
+                    s3.setUpdatedAt(1520010299);
+                    //s3.setContent();
+                    s3.setWatch(true);
+                    s3.setComment(true);
+                    s3.setWatchCount(111111111);
+                    s3.setCommentCount(2);
+                    suggestList.add(s1);
+                    suggestList.add(s2);
+                    suggestList.add(s3);
+                    suggestList.add(s1);
+                    suggestList.add(s2);
+                    suggestList.add(s3);
+                    suggestList.add(s1);
+                    suggestList.add(s2);
+                    suggestList.add(s3);
+                    recyclerManager.data(suggestList, more);
+                }
             }
         }, 1000);
-    }
-
-    private void getDataByHttp(boolean refresh) {
-        // todo api
-        SuggestAdapter adapter = (SuggestAdapter) rv.getAdapter();
-        if (refresh) {
-            List<Suggest> suggestList = new ArrayList<>();
-            Suggest s1 = new Suggest();
-            s1.setTitle("我发现了一个bug！");
-            s1.setCreatedAt(1520866299);
-            s1.setUpdatedAt(1520866299);
-            //s1.setContent();
-            s1.setWatch(false);
-            s1.setComment(false);
-            s1.setWatchCount(0);
-            s1.setCommentCount(0);
-            Suggest s2 = new Suggest();
-            s2.setTitle("我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！");
-            s2.setCreatedAt(1520010299);
-            s2.setUpdatedAt(1520866299);
-            //s2.setContent();
-            s2.setWatch(true);
-            s2.setComment(false);
-            s2.setWatchCount(111111111);
-            s2.setCommentCount(0);
-            Suggest s3 = new Suggest();
-            s3.setTitle("我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！我发现了一个bug！");
-            s3.setCreatedAt(1520010299);
-            s3.setUpdatedAt(1520010299);
-            //s3.setContent();
-            s3.setWatch(true);
-            s3.setComment(true);
-            s3.setWatchCount(111111111);
-            s3.setCommentCount(2);
-            suggestList.add(s1);
-            suggestList.add(s2);
-            suggestList.add(s3);
-            suggestList.add(s1);
-            suggestList.add(s2);
-            suggestList.add(s3);
-            suggestList.add(s1);
-            suggestList.add(s2);
-            suggestList.add(s3);
-            adapter.setData(suggestList);
-        } else {
-            Suggest s0 = new Suggest();
-            s0.setTitle("下拉出来的！");
-            s0.setCreatedAt(1520866299);
-            s0.setUpdatedAt(1520866299);
-            //s1.setContent();
-            s0.setWatch(false);
-            s0.setComment(false);
-            s0.setWatchCount(0);
-            s0.setCommentCount(0);
-            List<Suggest> list = new ArrayList<>();
-            list.add(s0);
-            adapter.addData(list);
-        }
     }
 
 }
--- app/src/main/java/com/jiangzg/ita/adapter/SuggestAdapter.java
+++ app/src/main/java/com/jiangzg/ita/adapter/SuggestAdapter.java
@@ -1,221 +1,111 @@
 package com.jiangzg.ita.adapter;
 
 import android.content.res.ColorStateList;
-import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.jiangzg.ita.R;
 import com.jiangzg.ita.domain.Suggest;
 import com.jiangzg.ita.utils.TimeUtils;
 import com.jiangzg.ita.utils.ViewUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Created by JZG on 2018/3/12.
+ * Created by JZG on 2018/3/13.
  * 意见反馈适配器
  */
-public class SuggestAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+public class SuggestAdapter extends BaseQuickAdapter<Suggest, BaseViewHolder> {
 
-    private FragmentActivity activity;
-    private List<Suggest> suggestList;
-
-    private static final int TYPE_ITEM = 0;
-    private static final int TYPE_HEAD = 1;
+    private FragmentActivity mActivity;
     private final int colorGrey;
     private final int colorPrimary;
 
     public SuggestAdapter(FragmentActivity activity) {
-        this.activity = activity;
-        suggestList = new ArrayList<>();
+        super(R.layout.list_item_suggest);
+        mActivity = activity;
         colorGrey = ContextCompat.getColor(activity, R.color.icon_grey);
         int rId = ViewUtils.getColorPrimary(activity);
         colorPrimary = ContextCompat.getColor(activity, rId);
     }
 
-    public void setData(List<Suggest> suggestList) {
-        this.suggestList = suggestList;
-        notifyDataSetChanged();
-    }
-
-    public void addData(List<Suggest> suggestList) {
-        int insert = this.suggestList.size();
-        this.suggestList.addAll(suggestList);
-        notifyItemInserted(insert);
-    }
-
     @Override
-    public int getItemCount() {
-        if (suggestList == null) {
-            return 1;
-        }
-        return suggestList.size() + 1;
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        if (position == 0) {
-            return TYPE_HEAD;
+    protected void convert(BaseViewHolder helper, Suggest item) {
+        // data
+        String title = item.getTitle();
+        long createdAt = item.getCreatedAt();
+        String create = TimeUtils.getSuggestShowBySecond(createdAt);
+        String createShow = String.format(mActivity.getString(R.string.create_at_holder), create);
+        long updatedAt = item.getUpdatedAt();
+        String update = TimeUtils.getSuggestShowBySecond(updatedAt);
+        String updatedShow = String.format(mActivity.getString(R.string.update_at_holder), update);
+        final int watchCount = item.getWatchCount();
+        String watchShow;
+        if (watchCount <= 0) {
+            watchShow = mActivity.getString(R.string.follow);
         } else {
-            return TYPE_ITEM;
+            watchShow = String.format("%d", watchCount);
         }
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == TYPE_HEAD) {
-            View view = LayoutInflater.from(activity).inflate(R.layout.list_head_suggest, parent, false);
-            return new HeadHolder(view);
+        int commentCount = item.getCommentCount();
+        String commentShow;
+        if (commentCount <= 0) {
+            commentShow = mActivity.getString(R.string.comment);
         } else {
-            View view = LayoutInflater.from(activity).inflate(R.layout.list_item_suggest, parent, false);
-            return new ItemHolder(view);
+            commentShow = String.format("%d", commentCount);
         }
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
-        if (holder instanceof HeadHolder) {
-            // data
-            //String desc = help.getDesc();
-            //List<Help.Content> contentList = help.getContentList();
-            //// view
-            //HeadHolder headHolder = (HeadHolder) holder;
-            //headHolder.tvDesc.setText(desc);
-            //// list
-            //headHolder.rv.setLayoutManager(new LinearLayoutManager(activity));
-            //HelpContentAdapter adapter = new HelpContentAdapter(activity, contentList);
-            //headHolder.rv.setAdapter(adapter);
+        final boolean watch = item.isWatch();
+        boolean comment = item.isComment();
+        // view
+        helper.setText(R.id.tvTitle, title);
+        helper.setText(R.id.tvCreateAt, createShow);
+        helper.setText(R.id.tvUpdateAt, updatedShow);
+        helper.setText(R.id.tvWatch, watchShow);
+        helper.setText(R.id.tvComment, commentShow);
+        if (watch) {
+            helper.setImageResource(R.id.ivWatch, R.drawable.ic_visibility_on_primary);
         } else {
-            ItemHolder itemHolder = (ItemHolder) holder;
-            // data
-            final Suggest suggest = suggestList.get(position - 1);
-            String title = suggest.getTitle();
-            long createdAt = suggest.getCreatedAt();
-            String create = TimeUtils.getSuggestShowBySecond(createdAt);
-            String createShow = String.format(activity.getString(R.string.create_at_holder), create);
-            long updatedAt = suggest.getUpdatedAt();
-            String update = TimeUtils.getSuggestShowBySecond(updatedAt);
-            String updatedShow = String.format(activity.getString(R.string.update_at_holder), update);
-            final int watchCount = suggest.getWatchCount();
-            String watchShow;
-            if (watchCount <= 0) {
-                watchShow = activity.getString(R.string.follow);
-            } else {
-                watchShow = String.format("%d", watchCount);
-            }
-            int commentCount = suggest.getCommentCount();
-            String commentShow;
-            if (commentCount <= 0) {
-                commentShow = activity.getString(R.string.comment);
-            } else {
-                commentShow = String.format("%d", commentCount);
-            }
-            final boolean watch = suggest.isWatch();
-            boolean comment = suggest.isComment();
-            // view
-            itemHolder.tvTitle.setText(title);
-            itemHolder.tvCreateAt.setText(createShow);
-            itemHolder.tvUpdateAt.setText(updatedShow);
-            itemHolder.tvWatch.setText(watchShow);
-            itemHolder.tvComment.setText(commentShow);
-            if (watch) {
-                itemHolder.ivWatch.setImageResource(R.drawable.ic_visibility_on_primary);
-            } else {
-                itemHolder.ivWatch.setImageResource(R.drawable.ic_visibility_off_grey);
-            }
-            if (comment) {
-                itemHolder.ivComment.setImageTintList(ColorStateList.valueOf(colorPrimary));
-            } else {
-                itemHolder.ivComment.setImageTintList(ColorStateList.valueOf(colorGrey));
-            }
-            // listener
-            itemHolder.llTitle.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // todo 详情页跳转
-                }
-            });
-            itemHolder.llWatch.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    boolean willWatch = !watch;
-                    suggest.setWatch(willWatch);
-                    int willWatchCount;
-                    if (willWatch) {
-                        willWatchCount = watchCount + 1;
-                    } else {
-                        willWatchCount = watchCount - 1;
-                    }
-                    if (willWatchCount <= 0) {
-                        willWatchCount = 0;
-                    }
-                    suggest.setWatchCount(willWatchCount);
-                    notifyItemChanged(position);
-                    // todo api
-                }
-            });
-            itemHolder.llComment.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // todo 详情页跳转 位置到评论
-                }
-            });
+            helper.setImageResource(R.id.ivWatch, R.drawable.ic_visibility_off_grey);
         }
+        ImageView ivComment = helper.getView(R.id.ivComment);
+        if (comment) {
+            ivComment.setImageTintList(ColorStateList.valueOf(colorPrimary));
+        } else {
+            ivComment.setImageTintList(ColorStateList.valueOf(colorGrey));
+        }
+        // listener
+        helper.addOnClickListener(R.id.llTitle);
+        helper.addOnClickListener(R.id.llWatch);
+        helper.addOnClickListener(R.id.llComment);
     }
 
-    // todo
-    public class HeadHolder extends RecyclerView.ViewHolder {
-
-        //TextView tvDesc;
-        //RecyclerView rv;
-
-        HeadHolder(View view) {
-            super(view);
-            //tvDesc = view.findViewById(R.id.tvDesc);
-            //rv = view.findViewById(R.id.rv);
-        }
+    public void goSuggestDetail(int position) {
+        Suggest item = getItem(position);
+        // todo 详情页跳转
     }
 
-    public class ItemHolder extends RecyclerView.ViewHolder {
-
-        CardView root;
-        LinearLayout llTitle;
-        TextView tvTitle;
-        TextView tvCreateAt;
-        TextView tvUpdateAt;
-        LinearLayout llWatch;
-        ImageView ivWatch;
-        TextView tvWatch;
-        LinearLayout llComment;
-        ImageView ivComment;
-        TextView tvComment;
-
-        ItemHolder(View view) {
-            super(view);
-            root = view.findViewById(R.id.root);
-            llTitle = view.findViewById(R.id.llTitle);
-            tvTitle = view.findViewById(R.id.tvTitle);
-            tvCreateAt = view.findViewById(R.id.tvCreateAt);
-            tvUpdateAt = view.findViewById(R.id.tvUpdateAt);
-            llWatch = view.findViewById(R.id.llWatch);
-            ivWatch = view.findViewById(R.id.ivWatch);
-            tvWatch = view.findViewById(R.id.tvWatch);
-            llComment = view.findViewById(R.id.llComment);
-            ivComment = view.findViewById(R.id.ivComment);
-            tvComment = view.findViewById(R.id.tvComment);
+    public void toggleWatch(int position) {
+        Suggest item = getItem(position);
+        boolean watch = item.isWatch();
+        int watchCount = item.getWatchCount();
+        boolean willWatch = !watch;
+        item.setWatch(willWatch);
+        int willWatchCount;
+        if (willWatch) {
+            willWatchCount = watchCount + 1;
+        } else {
+            willWatchCount = watchCount - 1;
         }
+        if (willWatchCount <= 0) {
+            willWatchCount = 0;
+        }
+        item.setWatchCount(willWatchCount);
+        notifyItemChanged(position);
+        // todo api
     }
 
+    public void comment(int position) {
+        // todo 详情页跳转 位置到评论
+    }
 }
