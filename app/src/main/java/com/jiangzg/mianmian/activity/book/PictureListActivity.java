7211b8f964c7c037407a18fd7068c70801012fd4
蒋治国 <18600757185@163.com>
2018-05-07 10:47:45 +0800

    Update pictureList

--- app/src/main/java/com/jiangzg/mianmian/activity/book/PictureListActivity.java
+++ app/src/main/java/com/jiangzg/mianmian/activity/book/PictureListActivity.java
@@ -2,14 +2,64 @@ package com.jiangzg.mianmian.activity.book;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.jiangzg.base.component.ActivityTrans;
 import com.jiangzg.mianmian.R;
 import com.jiangzg.mianmian.base.BaseActivity;
 import com.jiangzg.mianmian.domain.Album;
+import com.jiangzg.mianmian.helper.ConvertHelper;
+import com.jiangzg.mianmian.view.GImageView;
+import com.jiangzg.mianmian.view.GSwipeRefreshLayout;
+
+import java.util.Locale;
+
+import butterknife.BindView;
 
 public class PictureListActivity extends BaseActivity<PictureListActivity> {
+
+    @BindView(R.id.root)
+    CoordinatorLayout root;
+    @BindView(R.id.abl)
+    AppBarLayout abl;
+    @BindView(R.id.tb)
+    Toolbar tb;
+    @BindView(R.id.ctl)
+    CollapsingToolbarLayout ctl;
+
+    @BindView(R.id.rlHead)
+    RelativeLayout rlHead;
+    @BindView(R.id.ivCover)
+    GImageView ivCover;
+    @BindView(R.id.tvTitle)
+    TextView tvTitle;
+    @BindView(R.id.tvPictureCount)
+    TextView tvPictureCount;
+    @BindView(R.id.tvCreateAt)
+    TextView tvCreateAt;
+    @BindView(R.id.tvUpdateAt)
+    TextView tvUpdateAt;
+
+    @BindView(R.id.rv)
+    RecyclerView rv;
+    @BindView(R.id.srl)
+    GSwipeRefreshLayout srl;
+
+    @BindView(R.id.fabAdd)
+    FloatingActionButton fabAdd;
+
+    private Album album;
 
     public static void goActivity(Activity from, Album album) {
         Intent intent = new Intent(from, PictureListActivity.class);
@@ -25,12 +75,35 @@ public class PictureListActivity extends BaseActivity<PictureListActivity> {
 
     @Override
     protected void initView(Bundle state) {
-        // TODO 相册详情，顶部封面+详细信息(创建+更新+张数+我的+你的)，可压缩toolbar，照片列表背景是封面的虚化，且是瀑布流
+        // TODO 照片列表背景是封面的虚化，且是瀑布流
+
+        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
+        //mCollapsingToolbarLayout.setTitle("CollapsingToolbarLayout");
+        ////通过CollapsingToolbarLayout修改字体颜色
+        //mCollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        //mCollapsingToolbarLayout.setCollapsedTitleTextColor(Color.GREEN);//设置收缩后Toolbar上字体的颜色
     }
 
     @Override
     protected void initData(Bundle state) {
+        album = getIntent().getParcelableExtra("album");
+        refreshAlbumView();
+    }
 
+    private void refreshAlbumView() {
+        if (album == null) return;
+        // data
+        String cover = album.getCover();
+        String title = album.getTitle();
+        String createAt = ConvertHelper.getTimeShowCnSpace_HM_MD_YMD_ByGo(album.getCreateAt());
+        String createShow = String.format(Locale.getDefault(), getString(R.string.create_at_colon_space_holder), createAt);
+        String updateAt = ConvertHelper.getTimeShowCnSpace_HM_MD_YMD_ByGo(album.getUpdateAt());
+        String updateShow = String.format(Locale.getDefault(), getString(R.string.update_at_colon_space_holder), updateAt);
+        // view
+        ivCover.setDataOss(cover);
+        tvTitle.setText(title);
+        tvCreateAt.setText(createShow);
+        tvUpdateAt.setText(updateShow);
     }
 
 }
