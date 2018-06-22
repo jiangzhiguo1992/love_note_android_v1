cba5741c8afdc825519842c0e0225a3f679eb7f0
蒋治国 <18600757185@163.com>
2018-06-22 15:59:38 +0800

    Update homepager

--- app/src/main/java/com/jiangzg/mianmian/activity/HomeActivity.java
+++ app/src/main/java/com/jiangzg/mianmian/activity/HomeActivity.java
@@ -21,6 +21,7 @@ import com.jiangzg.mianmian.fragment.BookFragment;
 import com.jiangzg.mianmian.fragment.CoupleFragment;
 import com.jiangzg.mianmian.fragment.SquareFragment;
 import com.jiangzg.mianmian.fragment.TopicFragment;
+import com.jiangzg.mianmian.view.GScrollViewPager;
 
 import java.util.Stack;
 
@@ -29,7 +30,7 @@ import butterknife.BindView;
 public class HomeActivity extends BaseActivity<HomeActivity> {
 
     @BindView(R.id.vpContent)
-    ViewPager vpContent;
+    GScrollViewPager vpContent;
     @BindView(R.id.bnvBottom)
     BottomNavigationView bnvBottom;
 
--- app/src/main/java/com/jiangzg/mianmian/view/GScrollViewPager.java
+++ app/src/main/java/com/jiangzg/mianmian/view/GScrollViewPager.java
@@ -8,37 +8,44 @@ import android.view.MotionEvent;
 
 /**
  * Created by gg on 2017/4/3.
- * 取消侧滑的viewPager
+ * 必须侧滑的viewPager
  */
-public class GNoScrollViewPager extends ViewPager {
+public class GScrollViewPager extends ViewPager {
 
-    public GNoScrollViewPager(Context context) {
+    private float downX;
+
+    public GScrollViewPager(Context context) {
         super(context);
     }
 
-    public GNoScrollViewPager(Context context, AttributeSet attrs) {
+    public GScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     // 事件分发，是否分发给下级
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        getParent().requestDisallowInterceptTouchEvent(true); // 请求父控件不要拦截事件
         return super.dispatchTouchEvent(ev);
     }
 
     // 事件拦截，自己是否会处理
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        //return super.onInterceptTouchEvent(ev);
-        return true;
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = ev.getX();
+                return super.onInterceptTouchEvent(ev);
+            case MotionEvent.ACTION_MOVE:
+                float abs = Math.abs(ev.getX() - downX);
+                return abs >= 60 || super.onInterceptTouchEvent(ev);
+        }
+        return super.onInterceptTouchEvent(ev);
     }
 
     // 事件处理，自己要做啥处理
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        //return super.onTouchEvent(ev);
-        return true;
+        return super.onTouchEvent(ev);
     }
 }
