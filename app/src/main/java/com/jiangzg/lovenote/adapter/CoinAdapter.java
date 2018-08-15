5fa156a4e7075af9dc15c9b1f8d0b5780a3604dc
蒋治国 <18600757185@163.com>
2018-08-15 20:26:41 +0800

    Update coinList

---
+++ app/src/main/java/com/jiangzg/lovenote/adapter/CoinAdapter.java
@@ -0,0 +1,55 @@
+package com.jiangzg.lovenote.adapter;
+
+import android.support.v4.app.FragmentActivity;
+
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.jiangzg.lovenote.R;
+import com.jiangzg.lovenote.domain.Coin;
+import com.jiangzg.lovenote.domain.Couple;
+import com.jiangzg.lovenote.helper.ApiHelper;
+import com.jiangzg.lovenote.helper.CountHelper;
+import com.jiangzg.lovenote.helper.SPHelper;
+import com.jiangzg.lovenote.helper.TimeHelper;
+import com.jiangzg.lovenote.view.FrescoAvatarView;
+
+/**
+ * Created by JZG on 2018/3/13.
+ * 地址适配器
+ */
+public class CoinAdapter extends BaseMultiItemQuickAdapter<Coin, BaseViewHolder> {
+
+    private FragmentActivity mActivity;
+    private final Couple couple;
+
+    public CoinAdapter(FragmentActivity activity) {
+        super(null);
+        addItemType(ApiHelper.LIST_NOTE_MY, R.layout.list_item_coin_right);
+        addItemType(ApiHelper.LIST_NOTE_TA, R.layout.list_item_coin_left);
+        mActivity = activity;
+        couple = SPHelper.getCouple();
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Coin item) {
+        // data
+        String avatar = Couple.getAvatar(couple, item.getUserId());
+        String time = TimeHelper.getTimeShowLocal_HM_MDHM_YMDHM_ByGo(item.getCreateAt());
+        String kindShow = Coin.getKindShow(item.getKind());
+        String change;
+        if (item.getChange() >= 0) {
+            change = "+" + CountHelper.getShowCount2Thousand(item.getChange());
+        } else {
+            change = "-" + CountHelper.getShowCount2Thousand(item.getChange());
+        }
+        String count = CountHelper.getShowCount2Thousand(item.getCount());
+        // view
+        FrescoAvatarView ivAvatar = helper.getView(R.id.ivAvatar);
+        ivAvatar.setData(avatar);
+        helper.setText(R.id.tvTime, time);
+        helper.setText(R.id.tvKind, kindShow);
+        helper.setText(R.id.tvChange, change);
+        helper.setText(R.id.tvCount, count);
+    }
+
+}
--- app/src/main/java/com/jiangzg/lovenote/domain/Coin.java
+++ app/src/main/java/com/jiangzg/lovenote/domain/Coin.java
@@ -3,16 +3,69 @@ package com.jiangzg.lovenote.domain;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+import com.jiangzg.lovenote.R;
+import com.jiangzg.lovenote.base.MyApp;
+import com.jiangzg.lovenote.helper.ApiHelper;
+
 /**
  * Created by JZG on 2018/8/7.
  * Coin
  */
-public class Coin extends BaseCP implements Parcelable {
+public class Coin extends BaseCP implements Parcelable, MultiItemEntity {
+
+    public static final int COIN_KIND_ADD_BY_PLAY_PAY = 100; // +商店充值
+    public static final int COIN_KIND_ADD_BY_SIGN_DAY = 200; // +每日签到
+    public static final int COIN_KIND_ADD_BY_WIFE_POST = 310; // +发表夫妻
+    public static final int COIN_KIND_ADD_BY_LETTER_POST = 320; // +发表情书
+    public static final int COIN_KIND_ADD_BY_DISCUSS_POST = 330; // +发表讨论
+    public static final int COIN_KIND_SUB_BY_WIFE_UP = -310; // -夫妻投币
+    public static final int COIN_KIND_SUB_BY_LETTER_UP = -320; // -情书投币
+    public static final int COIN_KIND_SUB_BY_DISCUSS_UP = -330; // -讨论投币
+    public static final int COIN_KIND_SUB_BY_WISH_POST = -410; // -发表许愿
+    public static final int COIN_KIND_SUB_BY_WISH_BLESS = -411; // -许愿祝福
+    public static final int COIN_KIND_SUB_BY_PLANE_POST = -420; // -发表飞机
+    public static final int COIN_KIND_SUB_BY_PLANE_BLESS = -421; // -飞机祝福
 
     private int kind;
     private long billId;
     private int change;
     private int count;
+
+    public static String getKindShow(int form) {
+        switch (form) {
+            case COIN_KIND_ADD_BY_PLAY_PAY:
+                return MyApp.get().getString(R.string.play_pay);
+            case COIN_KIND_ADD_BY_SIGN_DAY:
+                return MyApp.get().getString(R.string.every_day_sign);
+            case COIN_KIND_ADD_BY_WIFE_POST:
+                return MyApp.get().getString(R.string.go_in_spae) + MyApp.get().getString(R.string.nav_wife);
+            case COIN_KIND_ADD_BY_LETTER_POST:
+                return MyApp.get().getString(R.string.go_in_spae) + MyApp.get().getString(R.string.nav_letter);
+            case COIN_KIND_ADD_BY_DISCUSS_POST:
+                return MyApp.get().getString(R.string.go_in_spae) + MyApp.get().getString(R.string.nav_discuss);
+            case COIN_KIND_SUB_BY_WIFE_UP:
+                return MyApp.get().getString(R.string.throw_coin_space) + MyApp.get().getString(R.string.nav_wife);
+            case COIN_KIND_SUB_BY_LETTER_UP:
+                return MyApp.get().getString(R.string.throw_coin_space) + MyApp.get().getString(R.string.nav_letter);
+            case COIN_KIND_SUB_BY_DISCUSS_UP:
+                return MyApp.get().getString(R.string.throw_coin_space) + MyApp.get().getString(R.string.nav_discuss);
+            case COIN_KIND_SUB_BY_WISH_POST:
+                return MyApp.get().getString(R.string.go_in_spae) + MyApp.get().getString(R.string.nav_wish);
+            case COIN_KIND_SUB_BY_WISH_BLESS:
+                return MyApp.get().getString(R.string.bless_space) + MyApp.get().getString(R.string.nav_wish);
+            case COIN_KIND_SUB_BY_PLANE_POST:
+                return MyApp.get().getString(R.string.go_in_spae) + MyApp.get().getString(R.string.nav_plane);
+            case COIN_KIND_SUB_BY_PLANE_BLESS:
+                return MyApp.get().getString(R.string.bless_space) + MyApp.get().getString(R.string.nav_plane);
+        }
+        return MyApp.get().getString(R.string.unknown_kind);
+    }
+
+    @Override
+    public int getItemType() {
+        return isMine() ? ApiHelper.LIST_NOTE_MY : ApiHelper.LIST_NOTE_TA;
+    }
 
     public int getKind() {
         return kind;
@@ -82,4 +135,5 @@ public class Coin extends BaseCP implements Parcelable {
             return new Coin[size];
         }
     };
+
 }
