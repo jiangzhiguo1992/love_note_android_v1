1177b874245fddae41f284a229f862dd35474cb8
蒋治国 <18600757185@163.com>
2018-08-20 20:49:24 +0800

    update

---
+++ app/src/main/java/com/jiangzg/lovenote/activity/more/MatchLetterActivity.java
@@ -0,0 +1,40 @@
+package com.jiangzg.lovenote.activity.more;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.jiangzg.base.component.ActivityTrans;
+import com.jiangzg.lovenote.R;
+import com.jiangzg.lovenote.base.BaseActivity;
+
+public class MatchLetterActivity extends BaseActivity<MatchLetterActivity> {
+
+    public static void goActivity(Activity from) {
+        Intent intent = new Intent(from, MatchLetterActivity.class);
+        // intent.putExtra();
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        ActivityTrans.start(from, intent);
+    }
+
+    @Override
+    protected int getView(Intent intent) {
+        return R.layout.activity_match_letter;
+    }
+
+    @Override
+    protected void initView(Intent intent, Bundle state) {
+
+    }
+
+    @Override
+    protected void initData(Intent intent, Bundle state) {
+
+    }
+
+    @Override
+    protected void onFinish(Bundle state) {
+
+    }
+
+}
--- app/src/main/java/com/jiangzg/lovenote/adapter/MatchLetterAdapter.java
+++ app/src/main/java/com/jiangzg/lovenote/adapter/MatchLetterAdapter.java
@@ -1,113 +1,82 @@
 package com.jiangzg.lovenote.adapter;
 
 import android.content.res.ColorStateList;
-import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.text.InputType;
-import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.jiangzg.base.common.ConvertUtils;
-import com.jiangzg.base.common.LogUtils;
-import com.jiangzg.base.common.StringUtils;
 import com.jiangzg.base.view.ScreenUtils;
 import com.jiangzg.lovenote.R;
-import com.jiangzg.lovenote.activity.common.BigImageActivity;
-import com.jiangzg.lovenote.activity.couple.CoupleInfoActivity;
-import com.jiangzg.lovenote.domain.MatchCoin;
-import com.jiangzg.lovenote.domain.MatchPoint;
-import com.jiangzg.lovenote.domain.MatchReport;
 import com.jiangzg.lovenote.domain.MatchWork;
-import com.jiangzg.lovenote.domain.Result;
-import com.jiangzg.lovenote.helper.API;
 import com.jiangzg.lovenote.helper.CountHelper;
-import com.jiangzg.lovenote.helper.DialogHelper;
-import com.jiangzg.lovenote.helper.RetrofitHelper;
 import com.jiangzg.lovenote.helper.ViewHelper;
-import com.jiangzg.lovenote.view.FrescoView;
-
-import retrofit2.Call;
 
 /**
  * Created by JZG on 2018/3/13.
- * wife适配器
+ * letter适配器
  */
-public class MatchWifeAdapter extends BaseQuickAdapter<MatchWork, BaseViewHolder> {
+public class MatchLetterAdapter extends BaseQuickAdapter<MatchWork, BaseViewHolder> {
 
     private FragmentActivity mActivity;
-    private final int width, height;
-    private final ColorStateList colorPrimaryStateList, colorWhiteStateList;
+    private final ColorStateList colorPrimaryStateList, colorGreyStateList;
 
-    public MatchWifeAdapter(FragmentActivity activity) {
-        super(R.layout.list_item_match_wife);
+    public MatchLetterAdapter(FragmentActivity activity) {
+        super(R.layout.list_item_match_letter);
         mActivity = activity;
-        width = ScreenUtils.getScreenWidth(activity) / 2;
-        height = ConvertUtils.dp2px(250);
         // color
         int rId = ViewHelper.getColorPrimary(activity);
         int colorPrimary = ContextCompat.getColor(activity, rId);
-        int colorWhite = ContextCompat.getColor(activity, R.color.white);
+        int colorGrey = ContextCompat.getColor(activity, R.color.icon_grey);
         colorPrimaryStateList = ColorStateList.valueOf(colorPrimary);
-        colorWhiteStateList = ColorStateList.valueOf(colorWhite);
+        colorGreyStateList = ColorStateList.valueOf(colorGrey);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, MatchWork item) {
         helper.setVisible(R.id.tvCover, item.isScreen() || item.isDelete());
         if (item.isScreen()) {
-            helper.setVisible(R.id.rlInfo, false);
+            helper.setVisible(R.id.llInfo, false);
             helper.setVisible(R.id.tvCover, true);
             helper.setText(R.id.tvCover, R.string.work_already_be_screen);
             return;
         } else if (item.isDelete()) {
-            helper.setVisible(R.id.rlInfo, false);
+            helper.setVisible(R.id.llInfo, false);
             helper.setVisible(R.id.tvCover, true);
             helper.setText(R.id.tvCover, R.string.work_already_be_delete);
             return;
         }
-        helper.setVisible(R.id.rlInfo, true);
+        helper.setVisible(R.id.llInfo, true);
         helper.setVisible(R.id.tvCover, false);
         // data
-        String contentImage = item.getContentImage();
+        String content = item.getTitle();
         String reportCount = CountHelper.getShowCount2Thousand(item.getReportCount());
         String pointCount = CountHelper.getShowCount2Thousand(item.getPointCount());
         String coinCount = CountHelper.getShowCount2Thousand(item.getCoinCount());
         boolean report = item.isReport();
         boolean point = item.isPoint();
         boolean coin = item.isCoin();
-        // image
-        FrescoView ivWork = helper.getView(R.id.ivWork);
-        ivWork.setWidthAndHeight(width, height);
-        ivWork.setData(contentImage);
+        // content
+        helper.setText(R.id.tvContent, content);
         // count
         helper.setText(R.id.tvReportCount, reportCount);
         helper.setText(R.id.tvPointCount, pointCount);
         helper.setText(R.id.tvCoinCount, coinCount);
         // user
         ImageView ivReport = helper.getView(R.id.ivReport);
-        ivReport.setImageTintList(report ? colorPrimaryStateList : colorWhiteStateList);
+        ivReport.setImageTintList(report ? colorPrimaryStateList : colorGreyStateList);
         ImageView ivPoint = helper.getView(R.id.ivPoint);
-        ivPoint.setImageTintList(point ? colorPrimaryStateList : colorWhiteStateList);
+        ivPoint.setImageTintList(point ? colorPrimaryStateList : colorGreyStateList);
         ImageView ivCoin = helper.getView(R.id.ivCoin);
-        ivCoin.setImageTintList(coin ? colorPrimaryStateList : colorWhiteStateList);
+        ivCoin.setImageTintList(coin ? colorPrimaryStateList : colorGreyStateList);
         // listener
         if (!item.isScreen() && !item.isDelete()) {
             helper.addOnClickListener(R.id.llReport);
             helper.addOnClickListener(R.id.llCoin);
             helper.addOnClickListener(R.id.llPoint);
         }
-    }
-
-    public void goWifeDetail(int position) {
-        MatchWork item = getItem(position);
-        if (item == null || item.isScreen() || item.isDelete()) return;
-        String contentImage = item.getContentImage();
-        BigImageActivity.goActivityByOss(mActivity, contentImage, null);
     }
 
 }
