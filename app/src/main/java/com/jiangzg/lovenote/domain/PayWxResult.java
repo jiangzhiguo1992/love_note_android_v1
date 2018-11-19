15921937502a29d62cf89d1f48fa17aa7386848f
蒋治国 <18600757185@163.com>
2018-11-19 13:49:50 +0800

    Update wxPay

--- app/src/main/java/com/jiangzg/lovenote/domain/PayWxResult.java
+++ app/src/main/java/com/jiangzg/lovenote/domain/PayWxResult.java
@@ -1,13 +1,10 @@
 package com.jiangzg.lovenote.domain;
 
-import com.tencent.mm.opensdk.constants.ConstantsAPI;
-
 /**
  * Created by JZG on 2018/11/3.
  */
 public class PayWxResult {
 
-    public static final int TYPE_WX = ConstantsAPI.COMMAND_PAY_BY_WX;
     public static final int CODE_OK = 0; // 成功
     public static final int CODE_ERR = -1; // 错误
     public static final int CODE_CANCEL = -2; // 用户取消
--- app/src/main/java/com/jiangzg/lovenote/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/jiangzg/lovenote/wxapi/WXPayEntryActivity.java
@@ -6,6 +6,7 @@ import com.jiangzg.lovenote.domain.PayWxResult;
 import com.jiangzg.lovenote.domain.RxEvent;
 import com.jiangzg.lovenote.helper.ConsHelper;
 import com.jiangzg.lovenote.helper.RxBus;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
 import com.tencent.mm.opensdk.modelbase.BaseReq;
 import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
@@ -22,7 +23,7 @@ public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
         PayWxResult result = new PayWxResult();
         result.setRespType(resp.getType());
         result.setErrCode(resp.errCode);
-        if (result.getRespType() == PayWxResult.TYPE_WX) {
+        if (result.getRespType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
             RxEvent<PayWxResult> event = new RxEvent<>(ConsHelper.EVENT_PAY_WX_RESULT, result);
             RxBus.post(event);
             finish();
