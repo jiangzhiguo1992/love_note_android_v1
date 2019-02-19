995c27715e95eb6903b5ea1d8ba79481d82428ee
蒋治国 <18600757185@163.com>
2019-02-19 15:29:31 +0800

    Update picture

--- app/src/main/java/com/jiangzg/lovenote/controller/adapter/note/PictureAdapter.java
+++ app/src/main/java/com/jiangzg/lovenote/controller/adapter/note/PictureAdapter.java
@@ -85,6 +85,7 @@ public class PictureAdapter extends BaseQuickAdapter<Picture, BaseViewHolder> {
             helper.setVisible(R.id.tvAddress, false);
         } else {
             helper.setVisible(R.id.tvHappenAt, item.getHappenAt() != 0);
+            helper.setVisible(R.id.vLine, !StringUtils.isEmpty(address));
             helper.setVisible(R.id.tvAddress, !StringUtils.isEmpty(address));
         }
         helper.setText(R.id.tvHappenAt, happen);
