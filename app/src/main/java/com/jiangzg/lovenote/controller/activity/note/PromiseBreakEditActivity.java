c91dcaa4b36af62514adf66faaa56ff52bdfc60e
蒋治国 <18600757185@163.com>
2019-03-26 13:44:18 +0800

    Add promiseBreak

---
+++ app/src/main/java/com/jiangzg/lovenote/controller/activity/note/PromiseBreakEditActivity.java
@@ -0,0 +1,15 @@
+package com.jiangzg.lovenote.controller.activity.note;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jiangzg.lovenote.R;
+
+public class PromiseBreakEditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promise_break_edit);
+    }
+}
