ea11ef25729e054babe28f8fb02d2b903a50a90d
蒋治国 <18600757185@163.com>
2019-04-12 22:13:14 +0800

    Add mensesInfoEdit

---
+++ app/src/main/java/com/jiangzg/lovenote/controller/activity/note/MensesInfoEditActivity.java
@@ -0,0 +1,15 @@
+package com.jiangzg.lovenote.controller.activity.note;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jiangzg.lovenote.R;
+
+public class MensesInfoEditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menses_info_edit);
+    }
+}
