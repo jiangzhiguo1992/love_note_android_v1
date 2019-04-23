51ea24d44571922fef4a913170af460595a9298b
蒋治国 <18600757185@163.com>
2019-04-23 20:18:50 +0800

    Update sleep

--- app/src/main/java/com/jiangzg/lovenote/controller/activity/note/SleepActivity.java
+++ app/src/main/java/com/jiangzg/lovenote/controller/activity/note/SleepActivity.java
@@ -14,7 +14,6 @@ import android.util.SparseIntArray;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
